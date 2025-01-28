d048ba7a29773e9dc1faaf2f6224ec0493697be0
leticiamfrota <128155229+leticiamfrota@users.noreply.github.com>
2025-01-28 23:34:17 +0000

    Utilizando Sintaxe Django

---
+++ models.py
@@ -0,0 +1,69 @@
+from django.db import models
+'''from django.contrib.auth.models import User'''
+
+
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=100)
+    password = models.CharField(max_length=128)
+
+class UserPortView(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    port = models.ForeignKey('Port', on_delete=models.CASCADE)
+    custom_name = models.CharField(max_length=100)
+        
+class UserPortGroup(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    port_group = models.ManyToManyField('Port', blank=True)
+
+class Device(models.Model):
+    mac = models.CharField(max_length=17)
+    name = models.CharField(max_length=100)
+
+class Port(models.Model):
+    PORT_CHOICES = [
+        ('NIVEL', 'Nível de Água'),
+        ('FLUXO', 'Fluxo de Água'),
+    ]
+    port_number = models.IntegerField()
+    port_type = models.CharField(
+        max_length=10, 
+        choices=PORT_CHOICES
+    )
+    device = models.ForeignKey('Device', on_delete=models.CASCADE)
+    config = models.JSONField(blank=True, null=True)
+
+class DataPort(models.Model):
+    time_stamp = models.DateTimeField()
+    value = models.FloatField()
+    std = models.FloatField()
+    port = models.ForeignKey('Port', on_delete=models.CASCADE)
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=100)
+    func = models.CharField(max_length=100)
+
+class Conta(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    supplier = models.ForeignKey('Supplier', on_delete=models.CASCADE)
+    expiration = models.DateField()
+    list_port = models.ManyToManyField('Port', blank=True)
+
+
+class Alert(models.Model):
+    ALERT_CHOICES = [
+        ('VAZAMENTO','Vazamento'),
+        ('SOBRECONSUMO','Sobreconsumo'),
+        ('FORA DO PADRÃO','Fora do padrão')
+    ]
+    alert_type = models.CharField(
+        max_length=100,
+        choices=ALERT_CHOICES
+    )
+    title = models.CharField(max_length=100)
+    mensagem = models.CharField(max_length=180)
+
+
+    
+
+
